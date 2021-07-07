54141aa6556be87121596e8d438434ef067e8038
tangdapeng <19950810295>
2021-07-07 17:10:18 +0800

    test

--- src/main/java/com/jiawa/wiki/WikiApplication.java
+++ src/main/java/com/jiawa/wiki/WikiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WikiApplication {
-
+q
     public static void main(String[] args) {
         SpringApplication.run(WikiApplication.class, args);
     }
