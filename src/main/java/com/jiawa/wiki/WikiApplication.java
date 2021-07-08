01e9e0949b6742e75928db51d10d5cee1bc45550
tangdapeng <19950810295>
2021-07-08 11:11:01 +0800

    修改启动日志

--- src/main/java/com/jiawa/wiki/WikiApplication.java
+++ src/main/java/com/jiawa/wiki/WikiApplication.java
@@ -1,13 +1,27 @@
 package com.jiawa.wiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
+/**
+ * @author MI
+ */
+
+//@ComponentScan({"com.jiawa","com.fasterxml"})
+@ComponentScan("com.jiawa")
 @SpringBootApplication
 public class WikiApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+        SpringApplication app = new SpringApplication(WikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
-
 }
