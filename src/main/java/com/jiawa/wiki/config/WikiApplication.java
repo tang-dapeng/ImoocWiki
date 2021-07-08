a91e7767a4ac6dcf578f23143ed895f7529c9957
tangdapeng <19950810295>
2021-07-08 11:55:52 +0800

    添加mybatis依赖

--- src/main/java/com/jiawa/wiki/config/WikiApplication.java
+++ src/main/java/com/jiawa/wiki/config/WikiApplication.java
@@ -1,8 +1,7 @@
-package com.jiawa.wiki;
+package com.jiawa.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
